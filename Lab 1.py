76920021631a29f517344d91b15204084dea516f
Andrew Kozempel <itzamk@gmail.com>
2023-08-31 00:35:44 -0400

    p3 and updates

--- Lab 1.py
+++ Lab 1.py
@@ -17,12 +17,13 @@ import time
 # worst case: O(n), n = min(num1,num2)
 def GCD(num1, num2):
 
+    # get start time
     start = time.time_ns()
 
     # test numbers ranging from the smaller number to 1
     for i in range(min(num1,num2), 0, -1):
 
-        # if both numbers are divisible by i, return i
+        # if both numbers are divisible by i, return i and total time
         if num1 % i == 0 and num2 % i == 0:
             return i, time.time_ns() - start
 
@@ -74,23 +75,24 @@ def find_max(list):
 
     start = time.time_ns()
 
-    # initialize min,max as first element for future comparisons
+    # initialize max as first element for future comparisons
     max = list[0]
 
     # iterate through all elements in the list
     for i in list:
 
-        # if i is larger than max, assign it as max
+        # if i is larger than max, i is new max
         if i > max:
             max = i
 
+    # return max and total time
     return max, time.time_ns() - start
 
 def find_min(list):
 
     start = time.time_ns()
 
-    # initialize min,max as first element for future comparisons
+    # initialize min as first element for future comparisons
     min = list[0]
 
     # iterate through all elements in the list
@@ -100,6 +102,7 @@ def find_min(list):
         if i < min:
             min = i
 
+    # return min and total time
     return min, time.time_ns() - start
 
 # test cases
@@ -114,9 +117,11 @@ for i in range(10000):
 # print min and max values
 print("\n\tPART 2: MIN AND MAX")
 for test in minmax_test_cases:
+
     #min_val, max_val, mm_time = min_max(test)
     min_val, min_time = find_min(test)
     max_val, max_time = find_max(test)
+
     print(f"\nTesting: {len(test)} elements \
           \nResult:   Min: {min_val}, Max: {max_val} \
           \nExpected: Min: {min(test)}, Max: {max(test)} \
@@ -126,3 +131,91 @@ for test in minmax_test_cases:
 ############################################################
 #  PART 3 - Min/Max
 ############################################################
+
+# O(n)
+def daq_max(list, left, right):
+
+    # if list has 1 element, return element
+    if left == right:
+        return list[left]
+    
+    # if list has 2 elements, return highest
+    elif right-left == 1:
+        if list[left] > list[right]:
+            return list[left]
+        else:
+            return list[right]
+        
+    else:
+
+        # find mid index
+        mid = (right + left) // 2
+
+        # recursively find max in each half
+        max_left = daq_max(list, left, mid)
+        max_right = daq_max(list, mid+1, right)
+
+        # return the max of both sides
+        if max_left > max_right:
+            return max_left
+        else:
+            return max_right
+
+# O(n)
+def daq_min(list, left, right):
+
+    # if list has 1 element, return element
+    if left == right:
+        return list[left]
+    
+    # if list has 2 elements, return lowest
+    elif right-left == 1:
+        if list[left] < list[right]:
+            return list[left]
+        else:
+            return list[right]
+        
+    else:
+
+        # find mid index
+        mid = (right + left) // 2
+
+        # recursively find max in each half
+        min_left = daq_min(list, left, mid)
+        min_right = daq_min(list, mid+1, right)
+
+        # return the min of both sides 
+        if min_left < min_right:
+            return min_left
+        else:
+            return min_right
+
+# test cases
+daq_test_cases = [[],[]]
+
+for i in range(1000):
+    daq_test_cases[0].append(random.randint(1,100000))
+
+for i in range(10000):
+    daq_test_cases[1].append(random.randint(1,100000))
+
+# print min and max values
+print("\n\tPART 3: DIVIDE AND CONQUER")
+for test in daq_test_cases:
+
+    right = len(test)-1
+    left = 0
+
+    start = time.time_ns()
+    daq_max_val = daq_max(test, left, right)
+    daq_max_time = time.time_ns() - start
+
+    start = time.time_ns()
+    daq_min_val = daq_min(test, left, right)
+    daq_min_time = time.time_ns() - start
+
+    print(f"\nTesting: {len(test)} elements \
+          \nResult:   Min: {daq_min_val}, Max: {daq_max_val} \
+          \nExpected: Min: {min(test)}, Max: {max(test)} \
+          \nMin Time: {daq_min_time} nanoseconds\
+          \nMax Time: {daq_max_time} nanoseconds")
