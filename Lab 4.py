0f6e81353ea51542b63b61a4891a2745284aa5bd
Andrew Kozempel <itzamk@gmail.com>
2023-10-12 11:45:49 -0400

    finish

--- Lab 4.py
+++ Lab 4.py
@@ -72,6 +72,26 @@ class PQ_LinkedList:
         self.head = None
         self.is_max = is_max
 
+    # str method to help with printing later
+    def __str__(self):
+
+        # current pointer
+        current = self.head
+        # list to store tuples
+        elements = []
+        
+        # if LL is empty
+        if self.head is None:
+            return '[]'
+        
+        # cycle through elements and add to list
+        while current is not None:
+            elements.append(f'({current.item}, {current.priority})')
+            current = current.next
+
+        # return elements
+        return "\t" + " -> ".join(elements)
+
     def insert(self, item, priority):
 
         # create new node based on element and priority
@@ -97,13 +117,13 @@ class PQ_LinkedList:
                 while current.next is not None and current.next.priority >= new_node.priority:
                     current = current.next
 
-                # add node to position you just determined
+                # add node to position you just determined (insert after current)
                 new_node.next = current.next
                 current.next = new_node
 
         else:
 
-            # if LL is empty or new node priority is greater than head's
+            # if LL is empty or new node priority is less than head's
             if self.head is None or new_node.priority < self.head.priority:
 
                 # insert new node at beginning
@@ -153,7 +173,7 @@ class PQ_LinkedList:
 
     def change_priority(self, item, new_priority):
 
-        # current pointer
+        # current and previous pointer
         current = self.head
         previous = None
         
@@ -243,7 +263,6 @@ class PQ_Heap:
 
     def change_priority(self, item, new_priority):
 
-        
         # iterate through elements in heap
         for i, (priority, ele) in enumerate(self.heap):
 
@@ -253,81 +272,100 @@ class PQ_Heap:
                 if self.is_max:
                     # set new priority based on index and rearrange
                     self.heap[i] = (-new_priority, item)
-                    heapq.heapify(self.heap)
-                    return
+                    break
                 
                 else:  
                     # set new priority based on index and rearrange
                     self.heap[i] = (new_priority, item)
-                    heapq.heapify(self.heap)
-                    return
-                
+                    break
+        
+        # reorganize to maintain heap property
+        heapq.heapify(self.heap)
+        return     
+    
+# EXAMPLES
 
-# Part 1: Array
-print("Part 1: Array")
+###### Part 1: Array
+print("\nPart 1: Array")
 max_pq_array = PQ_Array(is_max=True)
 min_pq_array = PQ_Array(is_max=False)
 
-# Insert
-max_pq_array.insert('A', 3)
-min_pq_array.insert('A', 3)
-
-# Peek
-print(max_pq_array.peek())  # Output: ('A', 3)
-print(min_pq_array.peek())  # Output: ('A', 3)
-
-# Change Priority
+print("\nMax Priority Queue (Array):")
+print("\tMax PQ Array:", max_pq_array.queue)
+max_pq_array.insert('A', 1)
+max_pq_array.insert('B', 2)
+print("\tMax PQ Array after insert:", max_pq_array.queue)
 max_pq_array.change_priority('A', 4)
-min_pq_array.change_priority('A', 2)
-print(max_pq_array.peek())  # Output: ('A', 4)
-print(min_pq_array.peek())  # Output: ('A', 2)
+print("\tMax PQ Array after priority change:", max_pq_array.queue)
+print("\tMax PQ Array Peak:", max_pq_array.peek())
+print("\tMax PQ Array deletion return value:",max_pq_array.delete())
+print("\tMax PQ Array after delete:", max_pq_array.queue)
 
-# Delete
-print(max_pq_array.delete())  # Output: ('A', 4)
-print(min_pq_array.delete())  # Output: ('A', 2)
+print("\nMin Priority Queue (Array):")
+print("\tMin PQ Array:", min_pq_array.queue)
+min_pq_array.insert('A', 1)
+min_pq_array.insert('B', 2)
+print("\tMin PQ Array after insert:", min_pq_array.queue)
+min_pq_array.change_priority('A', 4)
+print("\tMin PQ Array after priority change:", min_pq_array.queue)
+print("\tMin PQ Array Peek:", min_pq_array.peek())
+print("\tMin PQ Array deletion return value:", min_pq_array.delete())
+print("\tMin PQ Array after delete:", min_pq_array.queue)
 
-# Part 2: Linked List
+###### Part 2: Linked List
 print("\nPart 2: Linked List")
 max_pq_ll = PQ_LinkedList(is_max=True)
 min_pq_ll = PQ_LinkedList(is_max=False)
 
-# Insert
+print("\nMax Priority Queue (Linked List):")
+print('\tMax PQ LL:', max_pq_ll)
+max_pq_ll.insert('A', 1)
 max_pq_ll.insert('B', 2)
+print("\tMax PQ LL after insert:")
+print(max_pq_ll)
+max_pq_ll.change_priority('A', 4)
+print("\tMax PQ LL after priority change:")
+print(max_pq_ll)
+print("\tMax PQ LL Peek:", max_pq_ll.peek())
+print("\tMax PQ LL deletion return value:", max_pq_ll.delete())
+print("\tMax PQ LL after delete:", max_pq_ll)
+
+print("\nMin Priority Queue (Linked List):")
+print('\tMin PQ LL:', min_pq_ll)
+min_pq_ll.insert('A', 1)
 min_pq_ll.insert('B', 2)
+print("\tMin PQ LL after insert:")
+print(min_pq_ll)
+min_pq_ll.change_priority('A', 4)
+print("\tMin PQ LL after priority change:")
+print(min_pq_ll)
+print("\tMin PQ LL Peek:", min_pq_ll.peek())
+print("\tMin PQ LL deletion return value:", min_pq_ll.delete())
+print("\tMin PQ LL after delete:", min_pq_ll)
 
-# Peek
-print(max_pq_ll.peek())  # Output: ('B', 2)
-print(min_pq_ll.peek())  # Output: ('B', 2)
-
-# Change Priority
-max_pq_ll.change_priority('B', 3)
-min_pq_ll.change_priority('B', 1)
-print(max_pq_ll.peek())  # Output: ('B', 3)
-print(min_pq_ll.peek())  # Output: ('B', 1)
-
-# Delete
-print(max_pq_ll.delete())  # Output: ('B', 3)
-print(min_pq_ll.delete())  # Output: ('B', 1)
-
-# Part 3: Heap
+###### Part 3: Heap
 print("\nPart 3: Heap")
 max_pq_heap = PQ_Heap(is_max=True)
 min_pq_heap = PQ_Heap(is_max=False)
 
-# Insert
-max_pq_heap.insert('C', 1)
-min_pq_heap.insert('C', 1)
+print("\nMax Priority Queue (Heap):")
+print("\tMax PQ Heap:", max_pq_heap.heap)
+max_pq_heap.insert('A', 1)
+max_pq_heap.insert('B', 2)
+print("\tMax PQ Heap after insert:", max_pq_heap.heap)
+max_pq_heap.change_priority('A', 4)
+print("\tMax PQ Heap after priority change:", max_pq_heap.heap)
+print("\tMax PQ Heap Peek:", max_pq_heap.peek())
+print("\tMax PQ Heap deletion return value:", max_pq_heap.delete())
+print("\tMax PQ Heap after delete:", max_pq_heap.heap)
 
-# Peek
-print(max_pq_heap.peek())  # Output: ('C', 1)
-print(min_pq_heap.peek())  # Output: ('C', 1)
-
-# Change Priority
-max_pq_heap.change_priority('C', 2)
-min_pq_heap.change_priority('C', 0)
-print(max_pq_heap.peek())  # Output: ('C', 2)
-print(min_pq_heap.peek())  # Output: ('C', 0)
-
-# Delete
-print(max_pq_heap.delete())  # Output: ('C', 2)
-print(min_pq_heap.delete())  # Output: ('C', 0)
+print("\nMin Priority Queue (Heap):")
+print("\tMin PQ Heap:", min_pq_heap.heap)
+min_pq_heap.insert('A', 1)
+min_pq_heap.insert('B', 2)
+print("\tMin PQ Heap after insert:", min_pq_heap.heap)
+min_pq_heap.change_priority('A', 4)
+print("\tMin PQ Heap after priority change:", min_pq_heap.heap)
+print("\tMin PQ Heap Peek:", min_pq_heap.peek())
+print("\tMin PQ Heap deletion return value:", min_pq_heap.delete())
+print("\tMin PQ Heap after delete:", min_pq_heap.heap)
